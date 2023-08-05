1db4d334df5324ad802e2e454fcb81b3acda7b6e
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2023-08-05 20:58:53 +0100

    added stored output of commands such as ls into a variable

--- bash/ex1_variables.sh
+++ bash/ex1_variables.sh
@@ -21,3 +21,8 @@ echo "Random value: $RANDOM"
 
 #positional arguments follow the convention $1, $2, etc
 echo "Positional argument one: $1 , positional argument two: $2"
+
+#store command output using $(<command>) 
+ls_output=$(ls)
+
+echo "ls command output: $ls_output"
