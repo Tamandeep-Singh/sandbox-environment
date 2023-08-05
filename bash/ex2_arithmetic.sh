4d25b6b8f6dbca4664e46f55497f1ba6f5fe9b7b
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2023-08-05 21:11:07 +0100

    added exercise 2 for arithmetic in bash (evaluating expressions)

---
+++ bash/ex2_arithmetic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[sandbox-environment]: Script name -> ex2_arithmetic.sh"
+
+#format is let <varname>=<expression>
+let result=9+4
+
+#same principle with whitespace
+let "format_result = 9 + 4"
+
+echo "Unformatted result is: $result and formatted result is: $format_result"
+
+
+#expr prints the result directly (useful for storing straight into variables)
+expr 5 + 4
+
+a=$((4+6)) 
+
+echo "Using $, the value of a is: $a"
