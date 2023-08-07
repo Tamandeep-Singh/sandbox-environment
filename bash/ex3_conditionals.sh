7db1dee83a9e2235f6e75620540d110df9e09b1d
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2023-08-07 14:24:34 +0100

    Added another if statement to show how to make a directory if it doesn't exist

--- bash/ex3_conditionals.sh
+++ bash/ex3_conditionals.sh
@@ -21,4 +21,14 @@ else
     echo "Looks like the fruit isn't tasty"
 fi
 
+#use global env variable PWD to get full path for directory
+test_dir="$PWD/ex3_bash_test"
+
+#if the test directory does not exist then create it
+if [[ ! -d $test_dir ]]; then 
+   mkdir $test_dir
+   echo "$test_dir was created"
+else
+    echo "$test_dir already exists"
+fi
 
