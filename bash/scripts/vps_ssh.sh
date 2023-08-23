93964af2880e26ade7efbb561274b8655ffa521e
Tamandeep Singh <tamandeep.singh.business@gmail.com>
2023-08-23 14:34:19 +0100

    added functionality for inserting a document into a mongoDB collection on the remote host via ssh

--- bash/scripts/vps_ssh.sh
+++ bash/scripts/vps_ssh.sh
@@ -4,12 +4,19 @@
 
 #TODO: use a config file instead of variables embedded in the script itself
 username="root"
-host="<INSERT IP OR HOSTNAME ADDRESS HERE"
+host="<INSERT HOST IP OR HOSTNAME>"
 
-#ssh into remote host and perform the following bash commands through the use of EOF 
-#EOF treats the commands as a single string and is used as a heredoc 
-ssh "$username@$host" /bin/bash  << 'EOF'
-   echo "Current directory is $PWD"
+#ssh into remote host and perform the following bash commands through the use of EOF
+#EOF treats the commands as a single string and is used as a heredoc
+
+ssh "$username@$host" /bin/bash  << EOF
+   echo "Starting mongo shell"
+   #connect to mongo shell using username & password and use a nested heredoc to insert a new document into the connections collection
+   mongosh --shell -u "<INSERT MONGO USERNAME>" -p "<INSERT MONGO PASSWORD>" << MONGOEOF
+   use ssh_tests
+   db.connections.insertOne({host_ip: "$host", testID:"$RANDOM"})
+   quit()
+MONGOEOF
    echo "Exiting SSH session on remote host"
    exit
 EOF
